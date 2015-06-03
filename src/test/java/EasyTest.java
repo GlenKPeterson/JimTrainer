67ae5c451e9909b86aac6d9dba124cedd36d07fa
Glen K. Peterson <glen@organicdesign.org>
2015-06-02 22:06:30 -0400

    Updated all tests for latest UncleJim.

--- src/test/java/EasyTest.java
+++ src/test/java/EasyTest.java
@@ -1,86 +1,102 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.organicdesign.fp.ephemeral.View;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnit4.class)
 public class EasyTest {
-    // This test should pass as-is to prove that everything is installed correctly
-    @Test public void helloWorld() {
-        assertArrayEquals(new String[] { "Hello World" },
-                View.ofArray("Hello World").toArray());
-        assertEquals("Hello World", "Hello World");
-    }
-    // Construct an array by passing individual arguments to View
-    @Test public void fromAndToArray() {
-//        assertArrayEquals(new String[] { "a", "b", "c" },
-//                View.ofArray(???).toArray());
+
+    // Nothing to do here.  Just keeps IDEs from forgetting the imports.
+    @Test public void forceImports() {
+        assertArrayEquals(new Integer[] { 1,2,3, }, new Integer[] { 1,2,3, });
+        assertEquals(1, 1);
     }
 
-    // Concatenate a second view (append)
+    // This test should pass as-is to prove that everything is installed correctly
+    @Test public void helloWorld() {
+//        assertArrayEquals(new String[] { "Hello", "World" },
+//                PersistentVector.of(???).toTypedArray());
+    }
+
+    // Construct an array by passing individual arguments to
+    @Test public void fromAndToArray() {
+//        assertArrayEquals(new String[]{"a", "b", "c"},
+//                          PersistentVector.of(???).toTypedArray());
+    }
+
+    // Append to that
     @Test public void append() {
-//        assertArrayEquals(new String[] { "a", "b", "c", "d", "e", "f" },
-//                View.ofArray("a", "b").append(View.ofArray(???)).toArray());
+//        assertArrayEquals(new String[]{"a", "b", "c", "d", "e", "f"},
+//                          PersistentVector.of("a", "b", "c")
+//                                  .append(???).toTypedArray());
     }
 
     // Concatenate a second view (prepend)
     @Test public void prepend() {
-//        assertArrayEquals(new String[] { "a", "b", "c", "d", "e", "f" },
-//                View.ofArray("d", "e", "f").prepend(???).toArray());
+//        assertArrayEquals(new String[]{"a", "b", "c", "d", "e", "f"},
+//                          PersistentVector.of("d", "e", "f")
+//                                  .prepend(???).toTypedArray());
     }
 
-    // Return only the first two items
+    // Return only the first items
     @Test public void take() {
-//        assertArrayEquals(new String[] { "a", "b" },
-//                View.ofArray("a", "b", "c", "d", "e", "f").???.toArray());
+//        assertArrayEquals(new String[]{"a", "b"},
+//                          PersistentVector.of("a", "b", "c", "d", "e", "f")
+//                                  .take(??).toTypedArray());
     }
 
-    // Return only the last three items
+    // Return only the last items
     @Test public void drop() {
-//        assertArrayEquals(new String[] { "d", "e", "f" },
-//                View.ofArray("a", "b", "c", "d", "e", "f").???.toArray());
+//        assertArrayEquals(new String[]{"d", "e", "f"},
+//                          PersistentVector.of("a", "b", "c", "d", "e", "f")
+//                                  .drop(???).toTypedArray());
     }
 
     // What are the input values for this anonymous increment function (lambda)?
     @Test public void mapPlus() {
-//        assertArrayEquals(new Integer[] { 2, 3, 4, 5 },
-//                View.ofArray(???).map(i -> i + 1).toArray());
+//        assertArrayEquals(new Integer[]{2, 3, 4, 5},
+//                          PersistentVector.of(???)
+//                                  .map(i -> i + 1).toTypedArray());
     }
 
     // Fill in this anonymous decrement function (lambda)
     @Test public void mapMinus() {
-//        assertArrayEquals(new Integer[] { 2, 3, 4, 5 },
-//                View.ofArray(3, 4, 5, 6).map(i -> ???).toArray());
+//        assertArrayEquals(new Integer[]{2, 3, 4, 5},
+//                          PersistentVector.of(3, 4, 5, 6)
+//                                  .map(i -> ???).toTypedArray());
     }
 
-    // Write anonymous function that doubles integers
+    // Write an anonymous function that doubles integers
     @Test public void mapDouble() {
 //        assertArrayEquals(new Integer[]{2, 4, 6, 8},
-//                View.ofArray(1, 2, 3, 4).map(???).toArray());
+//                          PersistentVector.of(1, 2, 3, 4)
+//                                  .map(i -> i * 2).toTypedArray());
     }
 
-    // Write a function to accept numbers that match a certain test
+    // Write a function to accept only even numbers.  Hint: Use the modulo (%) operator.
     @Test public void filterEven() {
 //        assertArrayEquals(new Integer[]{2, 4, 6, 8},
-//                View.ofArray(1,2,3,4,5,6,7,8,9)
-//                        .filter(i -> ???).toArray());
+//                          PersistentVector.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
+//                                  .filter(i -> ???)
+//                                  .toTypedArray());
     }
 
     // Concatenate some strings - try using the new :: method pointer syntax
     @Test public void concat() {
 //        StringBuilder sB = new StringBuilder();
-//        View.ofArray("Little ", "strokes ", "fell ", "great ", "oaks.").forEach(???);
+//        PersistentVector.of("Little ", "strokes ", "fell ", "great ", "oaks.")
+//                .forEach(sB::???);
 //        assertEquals("Little strokes fell great oaks.", sB.toString());
     }
 
     // Chain together two higher order functions
     @Test public void takeAndDropChain() {
-//        assertArrayEquals(new String[] { "c" },
-//                          View.ofArray("a", "b", "c", "d", "e", "f")
-//                                  .drop(???).take(???).toArray());
+//        assertArrayEquals(new String[]{"c"},
+//                          PersistentVector.of("a", "b", "c", "d", "e", "f")
+//                                  .drop(???)
+//                                  .???(1).toTypedArray());
     }
 
 }
--- src/test/java/MediumTest.java
+++ src/test/java/MediumTest.java
@@ -9,55 +9,43 @@ import static org.junit.Assert.assertEquals;
 public class MediumTest {
 
     // Nothing to do here.  Just keeps IDEs from forgetting the imports.
-    @Test
-    public void forceImports() {
+    @Test public void forceImports() {
         assertArrayEquals(new Integer[] { 1,2,3, }, new Integer[] { 1,2,3, });
         assertEquals(1, 1);
     }
 
-    // Here we use map() to return a list of results for each input item.
-    @Test
-    public void twoDimenMap() {
-//        List<List<Integer>> result = new ArrayList<>();
-//        result.add(Arrays.asList(1, 2, 3));
-//        result.add(Arrays.asList(10, 20, 30));
-//        result.add(Arrays.asList(100, 200, 300));
-//        assertEquals(result,
-//                     View.ofArray(1, 10, 100)
-//                             .map(i -> Arrays.asList(i, i * 2, ???)).toJavaArrayList());
-    }
-
-    // flatMap returns several items for each input item (as opposed to a sub-list
-    // of items like the above example).  You could also think of it as "flattening"
-    // any sub-lists into a single one-dimensional list.
-    @Test
-    public void flatMap() {
-//        assertArrayEquals(new Integer[] { 1,2,3, 10,20,30, 100,200,300 },
-//                          View.ofArray(1, 10, 100)
-//                                  .flatMap(i -> View.ofArray(???)).toArray());
-    }
-
     // foldLeft takes a seed value and accumulates a result.
-    @Test
-    public void foldLeftPlus() {
+    @Test public void foldLeftPlus() {
 //        assertEquals((Integer) 15,
-//                     View.ofArray(1, 2, 3, 4, 5)
-//                             .foldLeft(0, (accum, item) -> ???));
+//                     PersistentVector.of(1, 2, 3, 4, 5)
+//                             .foldLeft(0, (accum, item) -> accum + ???));
     }
 
     // 0 was the identity value for "plus", what's the identity value for "times"?
-    @Test
-    public void foldLeftTimes() {
+    @Test public void foldLeftTimes() {
 //        assertEquals((Integer) 120,
-//                     View.ofArray(1, 2, 3, 4, 5)
+//                     PersistentVector.of(1, 2, 3, 4, 5)
 //                             .foldLeft(???, (accum, item) -> accum * item));
     }
 
-    // You can use foldLeft to do the same work as map() (but use map() in practice)
-    @Test
-    public void foldLeftMapLike() {
-//        assertArrayEquals(new Integer[] { 2, 3, 4, 5 },
-//                          View.ofArray(1, 2, 3, 4)
+    // Given the right seed value, foldLeft can find a maximum number
+    @Test public void findMax() {
+//        assertEquals((Integer) 73,
+//                PersistentVector.of(-201, 53, 73, 26, -59, 72)
+//                        .foldLeft(???, (max, i) -> (i > max) ? i : max));
+    }
+
+    // or a minimum
+    @Test public void findMin() {
+//        assertEquals(Integer.valueOf(-201),
+//                PersistentVector.of(-201, 53, 73, 26, -59, 72)
+//                        .foldLeft(Integer.MAX_VALUE, (min, i) -> ???));
+    }
+
+        // You can use foldLeft to do the same work as map() (but use map() in practice)
+    @Test public void foldLeftMapLike() {
+//        assertArrayEquals(new Integer[]{2, 3, 4, 5},
+//                          PersistentVector.of(1, 2, 3, 4)
 //                                  .foldLeft(new ArrayList<>(), (ls, i) -> {
 //                                      ???;
 //                                      return ls;
@@ -65,32 +53,36 @@ public class MediumTest {
     }
 
     // foldLeft can do the work of flatMap() too (but use flatMap() in practice)
-    @Test
-    public void foldLeftFlatMapLike() {
+    @Test public void foldLeftFlatMapLike() {
 //        assertArrayEquals(new Integer[] { 1,2,3, 10,20,30, 100,200,300 },
-//                          View.ofArray(1, 10, 100)
+//                          PersistentVector.of(1, 10, 100)
 //                                  .foldLeft(new ArrayList<>(), (ls, i) -> {
-//                                      ???;
-//                                      ???;
-//                                      ???;
+//                                      ls.add(???);
+//                                      ls.add(???);
+//                                      ls.add(???);
 //                                      return ls;
 //                                  }).toArray());
     }
 
-    // foldLeft can find a maximum number
-    @Test
-    public void findMax() {
-//        assertEquals((Integer) 73,
-//                View.ofArray(-201, 53, 73, 26, -59, 72)
-//                        .foldLeft(???, (max, i) -> (i > max) ? i : max));
+    // Here we use map() to return a list of results for each input item.
+    // This will result in a list of lists, AKA a 2-dimensional list
+    @Test public void twoDimenMap() {
+//        List<List<Integer>> result = new ArrayList<>();
+//        result.add(Arrays.asList(1, 2, 3));
+//        result.add(Arrays.asList(10, 20, 30));
+//        result.add(Arrays.asList(100, 200, 300));
+//
+//        assertArrayEquals(result.toArray(),
+//                          PersistentVector.of(1, 10, 100)
+//                                  .map(i -> unList(i, ???, ???)).toTypedArray());
     }
 
-    // or a minimum
-    @Test
-    public void findMin() {
-//        assertEquals(Integer.valueOf(-201),
-//                View.ofArray(-201, 53, 73, 26, -59, 72)
-//                        .foldLeft(Integer.MAX_VALUE, ???));
+    // flatMap returns several items for each input item (as opposed to a sub-list
+    // of items like the above example).  You could also think of it as "flattening"
+    // any sub-lists into a single one-dimensional list.
+    @Test public void flatMap() {
+//        assertArrayEquals(new Integer[] { 1,2,3, 10,20,30, 100,200,300 },
+//                          PersistentVector.of(1, 10, 100)
+//                                  .flatMap(i -> Sequence.of(?????)).toTypedArray());
     }
-
 }
